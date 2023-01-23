a1815109bbc5f11a012f10aa1ea1dc4cf64fe19f
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-01-23 12:25:15 -0500

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Parth Antala on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
